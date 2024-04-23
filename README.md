81643f74be41d680a1c0f1e59d084c50b599863f
Vaasu02 <chourasiavasu88@gmail.com>
2024-04-23 12:44:19 +0530

    InitialCommit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React + Vite
+
+HooBank is a futuristic bank. This project was created as a way to experiment with modern web development technologies, including the use of a UI library to speed up development.
+
+
+Build with:
+- React
+- Tailwind
+- Vite.js
---
+++ src/components/Clients.jsx
@@ -0,0 +1,16 @@
+import { clients } from "../constants";
+import styles from "../style";
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
+          <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[120px] xs:w-[140px] object-contain hover:bg-white/75 p-3 hover:rounded-md" />
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Clients;
